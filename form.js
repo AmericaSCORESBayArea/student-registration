252d7394f2604d77572f6c755b6d81f787f5e466
Kushaal-Tadepalli <Kushaal2@Kushaals-MBP.lan1>
2020-09-04 16:16:41 -0700

    include waivers dialog.

--- form.js
+++ form.js
@@ -21,4 +21,56 @@ window.onload = function Loader(){
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify(order));
     }
+
+
+    function setUpWaiver(id){
+        document.getElementById(id).value = document.getElementById('parent-first-name').value + ' '  + document.getElementById('parent-last-name').value
+        document.getElementById(id).disabled = true;
+    }
+
+
+    document.querySelector('#sign-waivers-btn').addEventListener('click', function(){
+        console.log('sign waivers');
+        document.getElementById('waiver-dialog').showModal();
+        setUpWaiver('esig-name1');
+        setUpWaiver('esig-name2');
+        setUpWaiver('esig-name3');
+        document.getElementById('submit-btn').disabled = false;
+    });
+
+    document.querySelector('#submit-waivers').addEventListener('click', function(){
+        checkWaiverSigs();
+        document.getElementById('waiver-dialog').close();   
+    });
+
+    document.querySelector('#cancel-waivers').addEventListener('click', function(){
+        document.getElementById('waiver-dialog').close();
+    });
+
+
+
+
+    function checkWaiverSigs(){
+        if(document.getElementById('ESig-agreement1').checked && document.getElementById('signature1').value == document.getElementById('esig-name1').value){
+            document.querySelector('#liability').checked = true;
+            document.querySelector('#liability').disabled = true;
+        }
+
+        if(document.getElementById('ESig-agreement2').checked && document.getElementById('signature2').value == document.getElementById('esig-name2').value){
+            document.querySelector('#data-release').checked = true;
+            document.querySelector('#data-release').disabled = true;
+        }
+
+        if(document.getElementById('ESig-agreement3').checked && document.getElementById('signature3').value == document.getElementById('esig-name3').value){
+            document.querySelector('#media-release').checked = true;
+            document.querySelector('#media-release').disabled = true;
+        }
+
+    }
+
+
+
+
+
+
 }
