fdb3004341081058e2d54e8ec53215d2a97ce7c8
rakesh tigulla <rtigulla@salesforce.com>
2021-03-18 00:31:56 -0400

    Made all the changes

--- public/js/form.js
+++ public/js/form.js
@@ -3,11 +3,24 @@ window.onload = function Loader(){
     document.getElementById('waiver-agree-btn').addEventListener('click', enableSubmit);
     document.getElementById('waiver-link').addEventListener('click', openModalPopup);
     document.getElementById('close-modal').addEventListener('click', closeModalPopup); 
-    
+    var acceptWaiver = false;
     function formData(){
         let data = {};
-        let form = document.getElementById('student-registration');
-        for(let el of form.elements){
+        let form = document.getElementById('form-fields');
+        for(let el of form.querySelectorAll("input")){
+            if(el.value.includes('--None') || el.value.includes('--N/A')){
+                data[el.title] = '';
+            }else if(el.title === 'MailingZip'){
+                if(el.value ==='' || el.value===undefined){
+                    data[el.title] = 0;
+                }else{
+                    data[el.title] = parseInt(el.value);
+                }
+            }else if(el.title !== ''){
+                data[el.title] = el.value;
+            }
+        }
+        for(let el of form.querySelectorAll("select")){
             if(el.value.includes('--None') || el.value.includes('--N/A')){
                 data[el.title] = '';
             }else if(el.title === 'MailingZip'){
@@ -42,23 +55,63 @@ window.onload = function Loader(){
                     console.log('success repsonse',response.data);
                     if(response.data !== undefined){
                         if(response.data.data === true){
-                           
+                            $( "#dialog-confirm" ).dialog({
+                                resizable: false,
+                                height: "auto",
+                                width: 400,
+                                modal: true,
+                                buttons: {
+                                  "Register New Student": function() {
+                                    $( this ).dialog( "close" );
+                                    resetFields();
+                                  },
+                                  "Goto Scores Home": function() {
+                                    window.location.href = "https://scoresu.org/";
+                                  }
+                                }
+                              });
                         }else{
-                            alert("Student Registration failed");
-                        }
+                            creationOfAlert("Student Registration Failed","Oops! Something Went Wrong in Registering the Student");
+                            }
                         
                         return response.data.contactId;
                     }else{
-                        alert("Student Registration failed");
+                        creationOfAlert("Student Registration Failed","Oops! Something Went Wrong in Registering the Student");
                         return '';
                     }
                     
                 }, (error) => {
-                    alert("Student Registration failed")
+                    creationOfAlert("Student Registration Failed","Oops! Something Went Wrong in Registering the Student");
                     return error;
                 });
     }
-        
+     
+    function resetFields(){
+        let form = document.getElementById("student-registration")
+        let unfilled = [];
+        form.reset();
+    }
+
+    function creationOfAlert(title,message){
+        var newDiv = $(document.createElement('div'));
+        newDiv.attr("title",title); 
+        var newSpan = $(document.createElement('span'));
+        newSpan.attr("class","ui-icon ui-icon-alert");
+        newSpan.attr("style","float:left; margin:1px 5px 0px 0;");
+        var newPara = $(document.createElement('p'));
+        newPara.append(newSpan,message);
+        newDiv.append(newPara);
+        $('bodyId').append(newDiv);
+        $(newDiv).dialog({
+            modal: true,
+            buttons: {
+                Ok: function() {
+                    $( this ).dialog( "close" );
+                }
+            }
+        });
+    }
+
     function submitForm(){
         let requiredFilled = true;
         let form = document.getElementById("student-registration")
@@ -72,40 +125,50 @@ window.onload = function Loader(){
         });
 
         if(!requiredFilled){
-            let titles = "\n";
+            let titles = "<br>";
             unfilled.forEach(function(i){
-                titles += i.title + "</br>";
+                titles += i.title + '<br>';
              });
-             alert('Please Fill the Required Fields'+ '<span class="red-req">*</span>' + title);
-        } else { 
-            /*$.confirm({
-                title: 'Student Registration',
-                content: 'Complete Registration?',
-                buttons: {
-                    confirm: function () {
-                        let postResponse = postFetch();
-                        console.log(postResponse);
-                    },
-                    cancel: function () {
-                    }
-                }
-            });*/
-            let complete = confirm('Complete Registration?');
-            if(complete){
-                let postResponse = postFetch();
-                console.log(postResponse);
-            }
-
+             creationOfAlert("",'<strong>Please Fill the Required Fields</strong><br>'+titles);
+        } else if(!acceptWaiver) {
+                creationOfAlert("","Please Read & Accept the Waiver");
+        }else { 
+            acceptWaiver = false;
+            confirmModal();
         }
     }
     
     function enableSubmit(){
-            document.getElementById("submit-btn").disabled = false;
+            acceptWaiver = true;
             closeModalPopup();
     }
 
-
-
+    function confirmModal(){
+        var newDiv = $(document.createElement('div'));
+        newDiv.attr("title","Confirmation"); 
+        var newSpan = $(document.createElement('span'));
+        newSpan.attr("class","ui-icon ui-icon-alert");
+        newSpan.attr("style","float:left; margin:10px 14px 0px 0;");
+        var newPara = $(document.createElement('p'));
+        newPara.append(newSpan,"Are you Sure?");
+        newDiv.append(newPara);
+        $('bodyId').append(newDiv);
+        $(newDiv).dialog({
+            resizable: false,
+            height: "auto",
+            width: 400,
+            modal: true,
+            buttons: {
+                "Yes": function() {
+                    $( this ).dialog( "close" );
+                    postFetch();
+                },
+                "No": function() {
+                  $( this ).dialog( "close" );
+                }
+            }
+        });
+    }
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
