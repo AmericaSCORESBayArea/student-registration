0d82b0f085f71557ad0608c3a8dffabc30478e97
m <m@m.com>
2021-04-24 10:39:17 -0400

    creating a general form element for text input field types

---
+++ ui/src/components/form/Text.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {nanoid} from "nanoid";
+import {FormGroup,Label,Input,FormText } from 'reactstrap';
+import FormLabel from "./Label";
+
+const TextFormElement = ({config,onValueChange,currentValue}) => {
+
+  if (!config) return null;
+  const {dataType, formValue, formLabel, helpText, placeholder,min,max} = config;
+  if (!dataType || !formValue) return null;
+  const elementId = nanoid();
+
+  return (
+    <FormGroup
+      title={`${!!helpText ? helpText : ``}`}
+    >
+      <FormLabel
+        config={config}
+        elementId={elementId}
+      />
+      <Input
+        id={`${elementId}`}
+        type={`${dataType}`}
+        name={`${formValue}`}
+        placeholder={`${!!placeholder ? placeholder : !!formLabel ? formLabel : formValue}`}
+        value={!!currentValue ? currentValue : ""}
+        onChange={onValueChange.bind(this, config)}
+        min={!!min ? min : ""}
+        max={!!max ? max : ""}
+      />
+    </FormGroup>
+  );
+};
+
+export default TextFormElement;
