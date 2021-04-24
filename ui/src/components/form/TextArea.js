3feb9933dd4217932bd851d37860a80568264ff8
m <m@m.com>
2021-04-24 10:39:06 -0400

    creating a general form element for text area field types

---
+++ ui/src/components/form/TextArea.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {nanoid} from "nanoid";
+import {FormGroup, Input} from 'reactstrap';
+import FormLabel from "./Label";
+
+const TextFormElement = ({config,onValueChange,currentValue}) => {
+
+  if (!config) return null;
+  const {dataType, formValue, formLabel, helpText, placeholder, min, max} = config;
+  if (!dataType || !formValue) return null;
+  const elementId = nanoid();
+
+  return (
+    <FormGroup
+      title={`${!!helpText ? helpText : ``}`}
+    >
+      <FormLabel
+        config={config}
+        elementId={elementId}
+      />
+      <Input
+        id={`${elementId}`}
+        type={`textarea`}
+        name={`${formValue}`}
+        placeholder={`${!!placeholder ? placeholder : !!formLabel ? formLabel : formValue}`}
+        value={!!currentValue ? currentValue : ""}
+        onChange={onValueChange.bind(this, config)}
+      />
+    </FormGroup>
+  );
+};
+
+export default TextFormElement;
