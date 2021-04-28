6df9bffc816511ba07e8e9a7b6bce959ff7f03d6
m <24796700+matthewjerome@users.noreply.github.com>
2021-04-28 14:02:49 -0400

    changing to 2-space indent, replacing var and let with const and simplifying the object creation of the mulesoft request body by using object deconstruction

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(bp.urlencoded({ extended: true }))
 
 
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({ path: './env/development.env' });
-  }
+  require('dotenv').config({path: './env/development.env'});
+}
 
 const id = process.env.CLIENT_ID;
 const secret = process.env.CLIENT_SECRET;
@@ -23,43 +23,45 @@ axios.defaults.headers.common['client_id'] = id;
 axios.defaults.headers.common['client_secret'] = secret;
 
 const reqHeaders = {
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
-
-var corsOptions = {
-    origin: baseUrl,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  headers: {
+    'Content-Type': 'application/json'
   }
+};
+
+const corsOptions = {
+  origin: baseUrl,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.post('/register',cors(corsOptions), async(req, res) => {
-    let data = req.body;
-    data["ContactRecordType"] = process.env.CONTACTRECORDID;
-    console.log('data',req.body);
-    let mRes =  await axios.post(muleEndPoint, data, reqHeaders)
+  const data = {
+    ...req.body,
+    ContactRecordType:process.env.CONTACTRECORDID
+  };
+  console.log('data', req.body);
+  const mRes = await axios.post(muleEndPoint, data, reqHeaders)
     .then((response) => {
-        console.log('success repsonse',response.data);
-        return response;
+      console.log('success repsonse', response.data);
+      return response;
     }, (error) => {
-        console.log(error);
-        console.log(error.data);
-        return error;
+      console.log(error);
+      console.log(error.data);
+      return error;
     });
-    if(mRes.isAxiosError){
-        res.status(mRes.response.status).json({ data: mRes.response.data});
-    }else{
-         var rData = mRes.data;
-         console.log(rData.Successful_Registration);
-        res.status(mRes.status).json({ data: rData.Successful_Registration});
-    }
-  }) 
-
-app.use(express.static(path.join(__dirname, '/public')));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, err => {
-    if(err) throw err;
-    console.log("%c Server running", "color: green");
+  if (mRes.isAxiosError) {
+    res.status(mRes.response.status).json({data: mRes.response.data});
+  } else {
+    const rData = mRes.data;
+    console.log(rData.Successful_Registration);
+    res.status(mRes.status).json({data: rData.Successful_Registration});
+  }
 });
 
+app.use(express.static(path.join(__dirname, '/public')));
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, err => {
+  if (err) throw err;
+  console.log("%c Server running", "color: green");
+});
