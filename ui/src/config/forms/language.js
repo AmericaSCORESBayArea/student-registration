d3c998e22c42a115f46fc82224cc63e38b0cc971
m <m@m.com>
2021-05-03 22:04:53 -0400

    creating a basic form definition for selecting language

---
+++ ui/src/config/forms/language.js
@@ -0,0 +1,15 @@
+const languageFormConfig = [
+  {
+    dataType:"enum",
+    formValue:"Language",
+    formLabel:"Language",
+    helpText:"Select Language",
+    placeholder:"Select Language",
+    enumItems: [
+      "English",
+      "Español"
+    ]
+  }
+];
+
+export default languageFormConfig;
