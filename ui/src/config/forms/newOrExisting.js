5d13ad4f88bdd500c5679d35923796120743424f
m <24796700+matthewjerome@users.noreply.github.com>
2021-05-04 14:21:59 -0400

    adding a form new vs existing registration

---
+++ ui/src/config/forms/newOrExisting.js
@@ -0,0 +1,15 @@
+const newOrExistingFormConfig = [
+  {
+    dataType:"enum",
+    formValue:"NewOrExisting",
+    formLabel:"New or Existing?",
+    helpText:"Select whether you are starting a new student registration or updating an existing student registration",
+    placeholder:"Select New  or Existing",
+    enumItems: [
+      "New",
+      "Existing"
+    ]
+  }
+];
+
+export default newOrExistingFormConfig;
