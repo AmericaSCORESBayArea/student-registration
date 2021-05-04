f4692979cde368033ad3b9cf9cae416eef99e0ff
m <m@m.com>
2021-05-03 22:05:24 -0400

    wrapping form definitions into a workflow list

---
+++ ui/src/config/workFlowConfig.js
@@ -0,0 +1,9 @@
+import languageFormConfig from "./forms/language";
+import registrationFormConfig from "./forms/registration";
+
+const workFlowConfig = [
+  languageFormConfig,
+  registrationFormConfig
+];
+
+export default workFlowConfig;
