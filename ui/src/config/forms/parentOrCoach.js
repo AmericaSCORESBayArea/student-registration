e1b2af4bdb845b92e4b0baad07f493db9bf1b523
m <24796700+matthewjerome@users.noreply.github.com>
2021-05-04 14:22:32 -0400

    adding a form for parent vs coach

---
+++ ui/src/config/forms/parentOrCoach.js
@@ -0,0 +1,15 @@
+const parentOrCoachFormConfig = [
+  {
+    dataType:"enum",
+    formValue:"ParentOfCoach",
+    formLabel:"Parent of Coach?",
+    helpText:"Select whether you are a parent or coach",
+    placeholder:"Select Parent or Coach",
+    enumItems: [
+      "Parent",
+      "Coach"
+    ]
+  }
+];
+
+export default parentOrCoachFormConfig;
