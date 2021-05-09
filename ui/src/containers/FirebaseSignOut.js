302341874a7c2b88b79491c805338f3c0ad46c89
m <24796700+matthewjerome@users.noreply.github.com>
2021-05-09 18:44:10 -0400

    creating a separate sign out button toolbar item component abstracted from the same existing firebase authentication form

---
+++ ui/src/containers/FirebaseSignOut.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { Fade, Button } from 'reactstrap';
+import SpinnerWithMessage from "../components/Spinner";
+
+const FirebaseSignOutContainer  = ({appConfig,onValueChange,currentValue}) => {
+  return (
+    <Button>{`button`}</Button>
+  )
+};
+
+export default FirebaseSignOutContainer;
