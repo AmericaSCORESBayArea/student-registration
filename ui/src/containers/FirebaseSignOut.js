add6bd113d234b4cd7e65584f9d69a825e07e479
m <24796700+matthewjerome@users.noreply.github.com>
2021-05-10 09:46:01 -0400

    creating a sign out button based on the authorization form state

--- ui/src/containers/FirebaseSignOut.js
+++ ui/src/containers/FirebaseSignOut.js
@@ -1,13 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import firebase from 'firebase';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { Fade, Button } from 'reactstrap';
-import SpinnerWithMessage from "../components/Spinner";
 
-const FirebaseSignOutContainer  = ({appConfig,onValueChange,currentValue}) => {
+const FirebaseSignOutContainer  = ({workflowState}) => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSigningOut, setIsSigningOut] = useState(false);
+
+  useEffect(() => {
+    if (!!workflowState) {
+      const authenticationFormState = workflowState.filter((item) => item.formName === "Authentication" && !!item.formState).pop();
+      if (!!authenticationFormState) {
+        setIsLoggedIn(true);
+      }
+    }
+    setIsLoading(false);
+  });
+
+  const signOutClickCallback = () => {
+    setIsSigningOut(true);
+    setIsLoading(true);
+    firebase.auth().signOut();
+    setTimeout(() => {
+      window.location = "/";
+    }, 1000);
+  };
+
   return (
-    <Button>{`button`}</Button>
-  )
+    isLoggedIn &&
+    <div>
+      <div className="clearfix" style={{padding: '.5rem'}}>
+        <Fade>
+          <Button
+            className="btn btn-secondary float-right"
+            onClick={signOutClickCallback}
+            disabled={isLoading || isSigningOut}
+          >{isSigningOut ? `Signing Out` : `Sign Out`}</Button>
+        </Fade>
+      </div>
+    </div>
+  );
 };
 
 export default FirebaseSignOutContainer;
