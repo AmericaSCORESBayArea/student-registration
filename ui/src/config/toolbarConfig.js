5af5fb6212d8d48c08ae2b888f0e3293d0acbfb7
m <24796700+matthewjerome@users.noreply.github.com>
2021-05-19 11:06:12 -0400

    adding toolbarConfig parameters for sign out and signing out text

--- ui/src/config/toolbarConfig.js
+++ ui/src/config/toolbarConfig.js
@@ -3,7 +3,9 @@ import FirebaseSignOutContainer from "../containers/FirebaseSignOut";
 const toolbarConfig = [
   {
     name: "firebaseSignOut",
-    Component: FirebaseSignOutContainer
+    Component: FirebaseSignOutContainer,
+    signOutButtonText:"!FIREBASE_AUTHENTICATION_SIGN_OUT_BUTTON_TEXT",
+    signingOutButtonText:"!FIREBASE_AUTHENTICATION_SIGNING_OUT_BUTTON_TEXT"
   }
 ];
 
