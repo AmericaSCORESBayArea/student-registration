c24c1a58a74ebdd44899fa81f25b21749344ab16
m <m@m.com>
2021-06-15 22:03:30 -0400

    removing localization from the workflow config

--- ui/src/config/workflowConfig.js
+++ ui/src/config/workflowConfig.js
@@ -1,21 +1,9 @@
-import languageFormConfig from "./forms/language";
 import authenticationFormConfig from "./forms/authentication";
 import parentOrCoachFormConfig from "./forms/parentOrCoach";
 import newOrExistingFormConfig from "./forms/newOrExisting";
 import registrationFormConfig from "./forms/registration";
 
 const workflowConfig = [
-  {
-    formName: "Language",
-    isStartingForm: true,
-    formConfig: languageFormConfig,
-    nextForm: "Authentication",
-    localStore:true,
-    submitOnValueChange:true,
-    breadCrumbLink:true,
-    breadCrumbPreviewFormStateValue:"Language",
-    isLocalizationForm:true
-  },
   {
     formName: "Authentication",
     formConfig: authenticationFormConfig,
