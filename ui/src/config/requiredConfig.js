7b091049ecab66257d880a06a9f80fe03da4f542
m <m@m.com>
2021-08-02 09:26:15 -0400

    creating a configuration to import strings related to required field

---
+++ ui/src/config/requiredConfig.js
@@ -0,0 +1,13 @@
+const requiredConfig = {
+  requiredText: "!REQUIRED_FIELD_REQUIRED_TEXT",
+  validText: "!REQUIRED_FIELD_VALID_TEXT",
+  text: "!REQUIRED_FIELD_TEXT_VALUE",
+  textArea: "!REQUIRED_FIELD_TEXT_VALUE",
+  enum: "!REQUIRED_FIELD_TEXT_VALUE",
+  email: "!REQUIRED_FIELD_EMAIL_VALUE",
+  number: "!REQUIRED_FIELD_NUMBER_VALUE",
+  tel: "!REQUIRED_FIELD_TELEPHONE_VALUE",
+  date: "!REQUIRED_FIELD_DATE_VALUE"
+};
+
+export default requiredConfig;
