8827bfceaa4429be5b45e5cc47143f51c44605ef
m <m@m.com>
2021-12-15 20:39:51 -0500

    adding button options images configuration

--- ui/src/components/form/ButtonOptions.js
+++ ui/src/components/form/ButtonOptions.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {nanoid} from "nanoid";
-import {Button, ButtonGroup, FormGroup} from 'reactstrap';
+import {Button, ButtonGroup, Card, CardBody, FormGroup, Row, Col} from 'reactstrap';
 import FormLabel from "./Label";
 
 const ButtonOptionsElement = ({config,onValueChange,currentValue,disabled}) => {
@@ -10,27 +10,30 @@ const ButtonOptionsElement = ({config,onValueChange,currentValue,disabled}) => {
     helpText,
     buttonOrientation,
     buttonItems,
-    size
+    size,
   } = config;
 
   const buttonDisplayValues = buttonItems.map((item) => typeof item === "object" ? !!item.displayValue ? item.displayValue : null : item).filter((item) => !!item);
   const buttonValues = buttonItems.map((item) => typeof item === "object" ? !!item.value ? item.value : null : item).filter((item) => !!item);
+  const buttonImages = buttonItems.map((item) => typeof item === "object" ? !!item.image ? item.image : null : item).filter((item) => !!item);
 
   const indexOfCurrentValueMatch = buttonValues.indexOf(currentValue);
   const currentDisplayValue = buttonValues.length > 0 && buttonValues.length === buttonDisplayValues.length && buttonValues.length > indexOfCurrentValueMatch ? buttonDisplayValues[indexOfCurrentValueMatch] : currentValue
 
   const elementId = nanoid();
   const onButtonItemClick = (buttonValue) => {
-    const indexOfMatch = buttonDisplayValues.indexOf(buttonValue);
-    const valueToPass = buttonValues.length > 0 && buttonValues.length === buttonDisplayValues.length && buttonValues.length > indexOfMatch ? buttonValues[indexOfMatch] : buttonValue;
-    if (indexOfMatch > -1) {
-      onValueChange(
-        {
-          target: {
-            value: valueToPass
+    if (!disabled) {
+      const indexOfMatch = buttonDisplayValues.indexOf(buttonValue);
+      const valueToPass = buttonValues.length > 0 && buttonValues.length === buttonDisplayValues.length && buttonValues.length > indexOfMatch ? buttonValues[indexOfMatch] : buttonValue;
+      if (indexOfMatch > -1) {
+        onValueChange(
+          {
+            target: {
+              value: valueToPass
+            }
           }
-        }
-      );
+        );
+      }
     }
   };
 
@@ -51,7 +54,35 @@ const ButtonOptionsElement = ({config,onValueChange,currentValue,disabled}) => {
       >
         {
           buttonDisplayValues.map((item, index) => {
-            return (
+            return buttonImages.length === buttonDisplayValues.length && buttonImages.length > index ?
+              <Card
+                key={index}
+                color={!!currentDisplayValue && item === currentDisplayValue ? "primary" : "secondary"}
+                onClick={onButtonItemClick.bind(this, item)}
+              >
+                <CardBody>
+                  <Row className="text-center">
+                    <Col xs={12}>
+                      <Button
+                        size={!!size ? size : "lg"}
+                        color={!!currentDisplayValue && item === currentDisplayValue ? "primary" : "secondary"}
+                        onClick={onButtonItemClick.bind(this, item)}
+                        disabled={disabled}
+                      >{`${item}`}</Button>
+                    </Col>
+                    <Col xs={12}>
+                      <img
+                        src={`/${buttonImages[index]}`}
+                        style={{
+                          width: "100%",
+                          maxHeight: "500px"
+                        }}
+                        alt={`${formValue} Image of ${buttonImages[index]}`}/>
+                    </Col>
+                  </Row>
+                </CardBody>
+              </Card>
+              :
               <Button
                 key={index}
                 size={!!size ? size : "lg"}
@@ -59,7 +90,6 @@ const ButtonOptionsElement = ({config,onValueChange,currentValue,disabled}) => {
                 onClick={onButtonItemClick.bind(this, item)}
                 disabled={disabled}
               >{`${item}`}</Button>
-            );
           })
         }
       </ButtonGroup>
