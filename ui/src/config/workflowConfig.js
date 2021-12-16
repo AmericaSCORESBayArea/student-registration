2a1947ce51024f4090c750b9ba282697585fedf7
m <m@m.com>
2021-12-16 02:11:18 -0500

    adding workflow decision based on selected region

--- ui/src/config/workflowConfig.js
+++ ui/src/config/workflowConfig.js
@@ -1,14 +1,10 @@
+import regionFormConfig from "./forms/region";
+import workflowConfigIFC from "./workflow/regions/ifc";
+import workflowConfigOGSC from "./workflow/regions/ogsc";
+import workflowConfigSFIC from "./workflow/regions/sfic";
 import authenticationFormConfig from "./forms/authentication";
-import parentOrCoachFormConfig from "./forms/parentOrCoach";
-import newOrExistingFormConfig from "./forms/newOrExisting";
-import registrationFormConfig from "./forms/registration";
 
 const workflowConfig = [
-  {
-    formName: "Select Region",
-    configEndpoint: "/region-config",
-    nextForm: "Authentication"
-  },
   {
     formName: "Authentication",
     formConfig: authenticationFormConfig,
@@ -16,37 +12,27 @@ const workflowConfig = [
     submitOnAnyValue:true
   },
   {
-    formName: "Parent or Coach",
-    formConfig: parentOrCoachFormConfig,
-    nextForm: "New or Existing",
+    formName: "Select Region",
+    formConfig: regionFormConfig,
     localStore:true,
     submitOnValueChange:true,
     breadCrumbLink:true,
-    breadCrumbPreviewFormStateValue:"ParentOrCoach"
-  },
-  {
-    formName: "New or Existing",
-    formConfig: newOrExistingFormConfig,
-    nextForm: "Registration",
-    localStore:true,
-    submitOnValueChange:true,
-    breadCrumbLink:true,
-    breadCrumbPreviewFormStateValue:"NewOrExisting"
-  },
-  {
-    formName: "Registration",
-    formConfig: registrationFormConfig,
-    postEndpoint: "/register",
-    displayWaiver: true,
-    waiverModalTitleText:"!REGISTRATION_FORM_WAIVER_MODAL_TITLE_TEXT",
-    waiverReviewAndAcceptRequestMessage:"!REGISTRATION_FORM_WAIVER_REVIEW_AND_ACCEPT_REQUEST_MESSAGE",
-    waiverAcceptButtonText:"!REGISTRATION_FORM_WAIVER_ACCEPT_BUTTON_TEXT",
-    waiverDeclineButtonText:"!REGISTRATION_FORM_WAIVER_DECLINE_BUTTON_TEXT",
-    waiverCloseButtonText:"!REGISTRATION_FORM_WAIVER_CLOSE_BUTTON_TEXT",
-    waiverShowWaiverButtonText:"!REGISTRATION_FORM_WAIVER_SHOW_WAIVER_BUTTON_TEXT",
-    waiverWaiverAcceptedMessage:"!REGISTRATION_FORM_WAIVER_WAIVER_ACCEPTED_MESSAGE",
-    displayWarnings: true,
-    breadCrumbLink:true
+    breadCrumbPreviewFormStateValue:"Region",
+    decisionMatchKey:"Region",
+    decisionValues: [
+      {
+        "decisionValue":"ifc",
+        "outputConfig":workflowConfigIFC
+      },
+      {
+        "decisionValue":"ogsc",
+        "outputConfig":workflowConfigOGSC
+      },
+      {
+        "decisionValue":"sfic",
+        "outputConfig":workflowConfigSFIC
+      }
+    ],
   }
 ];
 
