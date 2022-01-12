f8659499b729b458ccc1c8a9e896a6a56d39db28
m <m@m.com>
2022-01-11 23:40:34 -0500

    adding DateFormElement to form controller

--- ui/src/components/form/_controller.js
+++ ui/src/components/form/_controller.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextFormElement from "./Text";
+import DateFormElement from "./Date";
 import TextAreaFormElement from "./TextArea";
 import TitleFormElement from "./Title";
 import EnumerableElement from "./Enumerable";
@@ -16,55 +17,61 @@ const FormElementController = ({appConfig,config,onValueChange,onOverrideValueCh
       config={config}
     />
     :
-    dataType === "text" || dataType === "number" || dataType === "email" || dataType === "tel" || dataType === "date" ?
+    dataType === "text" || dataType === "number" || dataType === "email" || dataType === "tel" ?
       <TextFormElement
         config={config}
         onValueChange={onValueChange.bind(this, config)}
         currentValue={currentValue}
         disabled={disabled}
-      />
-      :
-      dataType === "textArea" ?
-        <TextAreaFormElement
+      /> :
+      dataType === "date" ?
+        <DateFormElement
           config={config}
           onValueChange={onValueChange.bind(this, config)}
           currentValue={currentValue}
           disabled={disabled}
-        />
-        :
-        dataType === "enum" ?
-          <EnumerableElement
+        /> :
+        dataType === "textArea" ?
+          <TextAreaFormElement
             config={config}
             onValueChange={onValueChange.bind(this, config)}
-            onOverrideValueChange={onOverrideValueChange.bind(this, config)}
             currentValue={currentValue}
-            currentOverrideValue={currentOverrideValue}
             disabled={disabled}
           />
           :
-          dataType === "buttonOptions" ?
-            <ButtonOptionsElement
+          dataType === "enum" ?
+            <EnumerableElement
               config={config}
               onValueChange={onValueChange.bind(this, config)}
+              onOverrideValueChange={onOverrideValueChange.bind(this, config)}
               currentValue={currentValue}
+              currentOverrideValue={currentOverrideValue}
               disabled={disabled}
             />
             :
-            dataType === "firebaseAuthentication" ?
-              <FirebaseAuthContainer
-                appConfig={appConfig}
+            dataType === "buttonOptions" ?
+              <ButtonOptionsElement
                 config={config}
                 onValueChange={onValueChange.bind(this, config)}
                 currentValue={currentValue}
+                disabled={disabled}
               />
               :
-              dataType === "static" ?
-                <StaticElement
+              dataType === "firebaseAuthentication" ?
+                <FirebaseAuthContainer
+                  appConfig={appConfig}
                   config={config}
+                  onValueChange={onValueChange.bind(this, config)}
                   currentValue={currentValue}
                 />
                 :
-                null
+                dataType === "static" ?
+                  <StaticElement
+                    config={config}
+                    currentValue={currentValue}
+                  />
+                  :
+                  null
 };
 
 export default FormElementController;
