72a12de187a7447fc9e5316317f83d66a8d3924e
m <m@m.com>
2022-01-30 12:12:37 -0500

    work in progress for updated workflow config

--- ui/src/config/forms/newOrExisting.js
+++ ui/src/config/forms/newOrExisting.js
@@ -1,21 +1,34 @@
+import registrationFormConfig from "./registration";
+
 const newOrExistingFormConfig = [
   {
-    dataType: "buttonOptions",
+    dataType: "searchWithNew",
     formValue: "NewOrExisting",
     formLabel: "!NEW_OR_EXISTING_FORM_LABEL_TEXT",
     helpText: "!NEW_OR_EXISTING_FORM_HELP_TEXT",
     placeholder: "",
     isRequired: true,
-    buttonItems: [
-      {
-        displayValue:"!NEW_OR_EXISTING_FORM_BUTTON_TEXT_NEW",
-        value:"New"
-      },
-      {
-        displayValue:"!NEW_OR_EXISTING_FORM_BUTTON_TEXT_EXISTING",
-        value:"Existing"
-      }
-    ]
+    searchConfig: {
+
+    },
+    existingConfig: {
+
+    },
+    newConfig:  {
+      formName: "Registration",
+      formConfig: registrationFormConfig,
+      postEndpoint: "/register",
+      displayWaiver: true,
+      waiverModalTitleText:"!REGISTRATION_FORM_WAIVER_MODAL_TITLE_TEXT",
+      waiverReviewAndAcceptRequestMessage:"!REGISTRATION_FORM_WAIVER_REVIEW_AND_ACCEPT_REQUEST_MESSAGE",
+      waiverAcceptButtonText:"!REGISTRATION_FORM_WAIVER_ACCEPT_BUTTON_TEXT",
+      waiverDeclineButtonText:"!REGISTRATION_FORM_WAIVER_DECLINE_BUTTON_TEXT",
+      waiverCloseButtonText:"!REGISTRATION_FORM_WAIVER_CLOSE_BUTTON_TEXT",
+      waiverShowWaiverButtonText:"!REGISTRATION_FORM_WAIVER_SHOW_WAIVER_BUTTON_TEXT",
+      waiverWaiverAcceptedMessage:"!REGISTRATION_FORM_WAIVER_WAIVER_ACCEPTED_MESSAGE",
+      displayWarnings: true,
+      breadCrumbLink:true
+    }
   }
 ];
 
