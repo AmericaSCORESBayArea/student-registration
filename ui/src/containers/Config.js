5b945d8a100c971ab7463b9de67c0fdf0b630d85
m <m@m.com>
2022-02-19 20:56:52 -0500

    fixing check defined before applying localization

--- ui/src/containers/Config.js
+++ ui/src/containers/Config.js
@@ -83,16 +83,18 @@ const ConfigContainer  = ({localizationValue}) => {
     return stringConfigInput;
   };
   const handleObjectLocalizedConfig = (objectConfigInput) => {
-    try {
-      let returnObj = {};
-      Object.keys(objectConfigInput).map((item) => {
-        const fieldNameToUse = blStartsWithKeyCharacter(item) ? getLocalizedValue(item) : item;
-        return returnObj[fieldNameToUse] = generateLocalizedConfig(objectConfigInput[item])
-      });
-      return returnObj;
-    } catch (e) {
-      console.error(`error in handleObjectLocalizedConfig`);
-      console.error(e);
+    if (objectConfigInput) {
+      try {
+        let returnObj = {};
+        Object.keys(objectConfigInput).map((item) => {
+          const fieldNameToUse = blStartsWithKeyCharacter(item) ? getLocalizedValue(item) : item;
+          return returnObj[fieldNameToUse] = generateLocalizedConfig(objectConfigInput[item])
+        });
+        return returnObj;
+      } catch (e) {
+        console.error(`error in handleObjectLocalizedConfig`);
+        console.error(e);
+      }
     }
     return objectConfigInput;
   };
