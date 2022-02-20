115d11439ff3f47a43b6899452050d05829cab20
m <m@m.com>
2022-02-19 22:17:06 -0500

    adding logic for showing modal on new registration

--- ui/src/containers/SearchWithNew.js
+++ ui/src/containers/SearchWithNew.js
@@ -4,11 +4,29 @@ import SpinnerWithMessage from "../components/Spinner";
 import { getAuth } from "firebase/auth";
 import axios from "axios";
 import {API_URL} from "./Config";
-import {Button, Card, Row, Col, Collapse, Table, Container} from "reactstrap";
+import {Button, Card, Row, Col, Collapse, Table, Container, Modal, ModalHeader} from "reactstrap";
+import FormContainer from "./Form";
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faPencil}  from  "@fortawesome/free-solid-svg-icons"
 
 const hiddenKeys = ["Id", "SchoolSiteId", "Name"]
 
-const PreviousDetailedContent = ({content,contentIndex}) => {
+const generateFormStateFromObj = (regObj) => {
+  let initState = []
+  if(regObj) {
+    Object.keys(regObj).map((item) => {
+      initState.push({
+        formValue:item,
+        value:regObj[item]
+      })
+    })
+  }
+  return initState
+}
+
+const PreviousDetailedContent = ({content,contentIndex,onEditClick}) => {
   const [isOpen, setIsOpen] = useState(false)
   const displayKeys = Object.keys(content).filter((item) => hiddenKeys.indexOf(item) === -1)
   return (
@@ -29,11 +47,7 @@ const PreviousDetailedContent = ({content,contentIndex}) => {
           <Row>
             <Col>
               <p/>
-              <Button
-                color={"primary"}
-                onClick={() => console.log("click")}
-              >{`Edit`}</Button>
-              <small style={{paddingLeft:"10px"}}>Click here to modify the registration.</small>
+                <small style={{paddingLeft:"10px"}}>Click on a pencil icon next to a value to modify</small>
               <p/>
             </Col>
           </Row>
@@ -41,18 +55,22 @@ const PreviousDetailedContent = ({content,contentIndex}) => {
             <Col>
               <Table striped={true}>
                 <tbody>
-                <tr>
                   {
                     displayKeys.map((item, index) => {
                       return (
                         <tr key={index}>
                           <td key={"key"}><b>{`${item}`}</b></td>
                           <td key={"value"}>{`${content[item]}`}</td>
+                          <td key={"edit"}>
+                            <Button
+                                color={"default"}
+                                onClick={() => onEditClick(contentIndex)}
+                            ><FontAwesomeIcon icon={faPencil} /></Button>
+                          </td>
                         </tr>
                       )
                     })
                   }
-                </tr>
                 </tbody>
               </Table></Col>
           </Row>
@@ -62,10 +80,13 @@ const PreviousDetailedContent = ({content,contentIndex}) => {
   )
 }
 
-const SearchWithNewContainer  = ({appConfig,onValueChange,currentValue}) => {
-
+const SearchWithNewContainer  = ({appConfig,requiredConfig,config,onValueChange,currentValue}) => {
+  const {editFormConfig} = config;
   const [loading, setLoading] = useState(false)
+  const [displayNewModal, setDisplayNewModal] = useState(false)
+  const [displayConfirmCancel, setDisplayConfirmCancel] = useState(false)
   const [previousRegistrations, setPreviousRegistrations] = useState([])
+  const [editingContentIndex, setEditingContentIndex] = useState(-1)
 
   useEffect(() => {
     const auth = getAuth();
@@ -85,11 +106,15 @@ const SearchWithNewContainer  = ({appConfig,onValueChange,currentValue}) => {
     }
   }, [])
 
-
   if (loading)
-    return (<SpinnerWithMessage
-      message={`Loading...`}
-    />)
+    return (
+      <SpinnerWithMessage
+        message={`Loading...`}
+      />
+    )
+
+  const confirmCancel = () => setDisplayConfirmCancel(true)
+
   return (
     <Container>
       <Row>
@@ -97,6 +122,7 @@ const SearchWithNewContainer  = ({appConfig,onValueChange,currentValue}) => {
           <Button
             size={"lg"}
             color={"primary"}
+            onClick={() => setDisplayNewModal(!displayNewModal)}
           >New</Button>
           <small style={{paddingLeft:"10px"}}>Click to register a new student</small>
         </Col>
@@ -114,12 +140,75 @@ const SearchWithNewContainer  = ({appConfig,onValueChange,currentValue}) => {
                   key={index}
                   content={item}
                   contentIndex={index}
+                  onEditClick={(contentIndex) => setEditingContentIndex(contentIndex)}
                 />
               )
             }
           </Col>
         </Row>
       }
+      <Modal
+        isOpen={displayNewModal}
+        size={"lg"}
+      >
+        <p/>
+        <Container style={{width:"100%"}}>
+          <Row>
+            <Col xs={12} sm={9}>
+              <h3>New Registration</h3>
+            </Col>
+            <Col xs={12} sm={3} style={{textAlign: "right"}}>
+              <Button
+                onClick={confirmCancel}
+              >X</Button>
+            </Col>
+          </Row>
+        </Container>
+        <div style={{padding:"20px 20px 20px 0"}}>
+          <FormContainer
+            requiredConfig={requiredConfig}
+            workflowConfig={editFormConfig}
+          />
+        </div>
+        <div
+          style={{
+            textAlign: "right",
+            padding:"10px"
+          }}
+        >
+          <Button
+            onClick={confirmCancel}
+          >Cancel</Button>
+          <p/>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={displayConfirmCancel}
+        size={"sm"}
+      >
+        <div style={{textAlign:"center"}}>
+          <p/>
+          <p/>
+          <h4>Are you sure you want to cancel without submitting?</h4>
+          <div>
+            <Button
+              size={"lg"}
+              onClick={() => {
+                setDisplayNewModal(false)
+                setDisplayConfirmCancel(false)
+              }}
+            >Yes</Button>{` `}
+            <Button
+                size={"lg"}
+                onClick={() => {
+                  setDisplayConfirmCancel(false)
+                }}
+            >No</Button>
+          </div>
+          <p/>
+          <p/>
+        </div>
+      </Modal>
     </Container>
   );
 };
