46402eacfaad26e5a9aaafa15d5acc4d3976ecfc
lautirozen <lautirozen@gmail.com>
2022-08-22 20:12:43 -0300

    binaries generated

--- ui/src/containers/Config.js
+++ ui/src/containers/Config.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
-import { Alert, Fade } from 'reactstrap';
-import axios from 'axios';
-import SpinnerWithMessage from '../components/Spinner';
+import React, { useState } from "react";
+import { Alert, Fade } from "reactstrap";
+import axios from "axios";
+import SpinnerWithMessage from "../components/Spinner";
 import WorkflowContainer from "./Workflow";
 import workflowConfig from "../config/workflowConfig";
 import toolbarConfig from "../config/toolbarConfig";
@@ -13,17 +13,16 @@ import localization_zh from "../config/strings/zh";
 
 const reqHeaders = {
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 };
 const localizationStrings = {
-  en:localization_en,
-  es:localization_es,
-  zh:localization_zh
+  en: localization_en,
+  es: localization_es,
+  zh: localization_zh,
 };
 
-const ConfigContainer  = ({localizationValue}) => {
-
+const ConfigContainer = ({ localizationValue }) => {
   const [configRequested, setConfigRequested] = useState(false);
   const [configRequestCompleted, setConfigRequestedCompleted] = useState(false);
   const [appConfig, setAppConfig] = useState(null);
@@ -33,19 +32,23 @@ const ConfigContainer  = ({localizationValue}) => {
 
   const runGetConfigRequest = () => {
     setConfigRequested(true);
-    axios.get('/info', {
-      header: reqHeaders
-    }).then(function (response) {
-      const {data} = response;
-      if (!!data) {
-        setAppConfig(data);
-      }
-    }).catch(function (error) {
-      console.error(error);
-      setRequestError(error.message);
-    }).then(function () {
-      setConfigRequestedCompleted(true);
-    });
+    axios
+      .get("/info", {
+        header: reqHeaders,
+      })
+      .then(function (response) {
+        const { data } = response;
+        if (!!data) {
+          setAppConfig(data);
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+        setRequestError(error.message);
+      })
+      .then(function () {
+        setConfigRequestedCompleted(true);
+      });
   };
 
   if (!configRequested) {
@@ -56,15 +59,17 @@ const ConfigContainer  = ({localizationValue}) => {
     }
   }
 
-  const blStartsWithKeyCharacter = (stringInput) => stringInput.indexOf(`!`) === 0;
+  const blStartsWithKeyCharacter = (stringInput) =>
+    stringInput.indexOf(`!`) === 0;
   const getLocalizedValue = (keyString) => {
     const lookupValue = keyString.slice(1);
     if (!!localizationStrings[localizationValue][lookupValue]) {
       return localizationStrings[localizationValue][lookupValue];
     }
     return keyString;
-  }
-  const handleArrayLocalizedConfig = (arrayConfigInput) => arrayConfigInput.map((item) => generateLocalizedConfig(item));
+  };
+  const handleArrayLocalizedConfig = (arrayConfigInput) =>
+    arrayConfigInput.map((item) => generateLocalizedConfig(item));
   const handleStringLocalizedConfig = (stringConfigInput) => {
     try {
       if (!!localizationValue) {
@@ -84,8 +89,12 @@ const ConfigContainer  = ({localizationValue}) => {
     try {
       let returnObj = {};
       Object.keys(objectConfigInput).map((item) => {
-        const fieldNameToUse = blStartsWithKeyCharacter(item) ? getLocalizedValue(item) : item;
-        return returnObj[fieldNameToUse] = generateLocalizedConfig(objectConfigInput[item])
+        const fieldNameToUse = blStartsWithKeyCharacter(item)
+          ? getLocalizedValue(item)
+          : item;
+        return (returnObj[fieldNameToUse] = generateLocalizedConfig(
+          objectConfigInput[item]
+        ));
       });
       return returnObj;
     } catch (e) {
@@ -97,13 +106,13 @@ const ConfigContainer  = ({localizationValue}) => {
 
   const generateLocalizedConfig = (configInput) => {
     try {
-      return Array.isArray(configInput) ?
-        handleArrayLocalizedConfig(configInput) :
-        typeof configInput === "string" ?
-          handleStringLocalizedConfig(configInput) :
-          typeof configInput === "object" ?
-            handleObjectLocalizedConfig(configInput) :
-            configInput;
+      return Array.isArray(configInput)
+        ? handleArrayLocalizedConfig(configInput)
+        : typeof configInput === "string"
+        ? handleStringLocalizedConfig(configInput)
+        : typeof configInput === "object"
+        ? handleObjectLocalizedConfig(configInput)
+        : configInput;
     } catch (e) {
       console.error(`error in generateLocalizedConfig`);
       console.error(e);
@@ -117,23 +126,14 @@ const ConfigContainer  = ({localizationValue}) => {
   const requiredConfigLocalized = generateLocalizedConfig(requiredConfig);
 
   return (
-    <Fade
-      in={true}
-    >
-      {
-        requestError.length > 0 &&
-        <Alert
-          color="danger"
-        >{`${requestError}`}</Alert>
-      }
-      {
-        configRequested && !configRequestCompleted &&
-        <SpinnerWithMessage
-          message={`Loading...`}
-        />
-      }
-      {
-        !!appConfig &&
+    <Fade in={true}>
+      {requestError.length > 0 && (
+        <Alert color="danger">{`${requestError}`}</Alert>
+      )}
+      {configRequested && !configRequestCompleted && (
+        <SpinnerWithMessage message={`Loading...`} />
+      )}
+      {!!appConfig && (
         <div>
           <WorkflowContainer
             appConfig={appConfigLocalized}
@@ -142,7 +142,7 @@ const ConfigContainer  = ({localizationValue}) => {
             requiredConfig={requiredConfigLocalized}
           />
         </div>
-      }
+      )}
     </Fade>
   );
 };
